95b3db6f073e964a99cf5d2368c8420ee278978e
Faahm <fatmahinay99@gmail.com>
2024-06-26 11:35:45 +0800

    update user model

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  email: { type: String, required: true, select: false, unique: true },
+  password: { type: String, select: false, required: true },
 });
 
 const User = mongoose.model("User", UserSchema);
